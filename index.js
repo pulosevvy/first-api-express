63a7a22f166556537242f332280f77b1da88a805
pulosevvy <107025377+pulosevvy@users.noreply.github.com>
2022-11-27 21:20:56 +0700

    git init

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import router from "./Router.js";
+import fileUpload from "express-fileupload";
+
+const PORT = 5000;
+const DB_URL = `mongodb+srv://admin:f7T34bXVExtZojz9@cluster0.67ptndg.mongodb.net/?retryWrites=true&w=majority`
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('static'));
+app.use(fileUpload({}));
+app.use('/api', router);
+
+
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true});
+        app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+
+startApp();
